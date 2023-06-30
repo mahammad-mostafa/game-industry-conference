c1b7cca0d9a541be171ae2943d7de6a3413343d3
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-01 00:49:26 +0300

    Loading speakers list dynamically

---
+++ assets/scripts/main.js
@@ -0,0 +1,57 @@
+const speakersList = document.querySelector('.speakers-list');
+const speakers = [
+  {
+    image: 'assets/images/speakers/wesley.jpg',
+    name: 'Wesley Adams',
+    company: 'Unity Technologies',
+    description: 'Wes has worked in games for 12 years, trying to find ways to make it easier for people to translate their creative visions into reality.',
+  },
+  {
+    image: 'assets/images/speakers/zev.jpg',
+    name: 'Zev Solomon',
+    company: 'Sony Interactive Entertainment',
+    description: 'Zev has been supporting developers launching their games and integrating PlayStation Network services from PS3 to PS5.',
+  },
+  {
+    image: 'assets/images/speakers/ian.jpg',
+    name: 'Ian Andes',
+    company: 'NVIDIA',
+    description: 'Ian Andes is a seasoned professional with over 25 years of experience in the Media and Entertainment industry.',
+  },
+  {
+    image: 'assets/images/speakers/francisco.jpg',
+    name: 'Francisco Cabrita',
+    company: 'Epic Games',
+    description: 'Francisco Cabrita is a seasoned design director with over 25 years’ experience in game development and design.',
+  },
+  {
+    image: 'assets/images/speakers/marcus.jpg',
+    name: 'Marcus Montgomery',
+    company: 'Xbox Games Studios Publishing',
+    description: 'Marcus Montgomery initially began his professional journey into video game development as a game tester at Sega of America.',
+  },
+  {
+    image: 'assets/images/speakers/john.jpg',
+    name: 'John Hartwig',
+    company: 'AMD',
+    description: 'John Hartwig is a Senior Member of the Technical Staff and is the CPU Team Lead in AMD\'s Game Engineering organization.',
+  },
+];
+function fillSpeakers() {
+  const fragment = new DocumentFragment();
+  speakers.forEach((speaker) => {
+    const article = document.createElement('article');
+    let articelHtml = '<span></span>';
+    articelHtml += `<img src="${speaker.image}" alt="${speaker.name}" />`;
+    articelHtml += '<div>';
+    articelHtml += `<h3 class="dark-color">${speaker.name}</h3>`;
+    articelHtml += `<i class="accent-color">${speaker.company}</i>`;
+    articelHtml += '<hr />';
+    articelHtml += `<p>${speaker.description}</p>`;
+    articelHtml += '</div>';
+    article.innerHTML = articelHtml;
+    fragment.appendChild(article);
+  });
+  speakersList.appendChild(fragment);
+}
+fillSpeakers();
